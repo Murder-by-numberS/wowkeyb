ed5fcc7ae0f481d19043173e2fd0126b7ecd5729
Logan Born <lcborn4@gmail.com>
2024-07-18 13:25:15 -0400

    its in browser folder now

--- server.js
+++ server.js
@@ -4,18 +4,17 @@ const app = express();
 const path = require('path');
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(`${__dirname}/dist/fuse/`));
+app.use(express.static(`${__dirname}/dist/fuse/browser`));
 
 // Nice and done!
 app.get('/backend', (req, res) => {
-  res.send({ url: process.env.BE_API });
+    res.send({ url: process.env.BE_API });
 });
-
 
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(`${__dirname}/dist/fuse/index.html`));
+    res.sendFile(path.join(`${__dirname}/dist/fuse/browser/index.html`));
 });
 
 // Start the app by listening on the default
