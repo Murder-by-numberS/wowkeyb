6c87304021f0c713aac80352c00cc81e6f4ca884
Logan Born <lcborn4@gmail.com>
2024-08-05 23:58:30 -0400

    removed the forward slash

--- src/environments/environment.develop.ts
+++ src/environments/environment.develop.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
     envName: 'develop',
-    apiUrl: 'http://develop-api.wowkeyb.gg/',
+    apiUrl: 'http://develop-api.wowkeyb.gg',
     hmr: false,
 };
--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
     envName: 'production',
-    apiUrl: 'https://api.wowkeyb.gg/',
+    apiUrl: 'https://api.wowkeyb.gg',
     hmr: false,
 };
