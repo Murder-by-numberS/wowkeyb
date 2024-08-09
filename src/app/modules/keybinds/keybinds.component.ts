90f65849c9c568b016db6dcd7b7735e088cafe1e
Logan Born <lcborn4@gmail.com>
2024-08-08 21:41:27 -0400

    smaller footer and keybinding page setup

--- src/app/modules/keybinds/keybinds.component.ts
+++ src/app/modules/keybinds/keybinds.component.ts
@@ -1,18 +1,31 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, ViewEncapsulation, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { RouterLink } from '@angular/router';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @Component({
     selector: 'keybinds',
     templateUrl: './keybinds.component.html',
     encapsulation: ViewEncapsulation.None,
     standalone: true,
-    imports: [MatButtonModule, RouterLink, MatIconModule],
+    imports: [
+        RouterLink,
+        MatButtonModule,
+        MatIconModule,
+        MatMenuModule,
+        MatSidenavModule
+    ],
 })
-export class KeybindsComponent {
+export class KeybindsComponent implements OnInit {
+    opened: boolean;
     /**
      * Constructor
      */
     constructor() { }
+
+    ngOnInit(): void {
+        this.opened = true;
+    }
 }
