2322aab1a83128bde9274a4a2df89e55136233fc
Logan Born <lcborn4@gmail.com>
2024-08-27 18:19:52 -0400

    ngx-panzoom 18

--- src/app/modules/keybinds/keyboard/keyboard.component.ts
+++ src/app/modules/keybinds/keyboard/keyboard.component.ts
@@ -1,9 +1,14 @@
-import { Component, ViewEncapsulation, OnInit } from '@angular/core';
+import { Component, ViewEncapsulation, OnInit, viewChild, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
+
+import { NgxPanZoomModule } from 'ngx-panzoom';
+import { PanZoomComponent, PanZoomModel } from 'ngx-panzoom';
+
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatSidenavModule } from '@angular/material/sidenav';
+
 
 @Component({
     selector: 'keyboard',
@@ -16,9 +21,13 @@ import { MatSidenavModule } from '@angular/material/sidenav';
         MatIconModule,
         MatMenuModule,
         MatSidenavModule,
+        NgxPanZoomModule
     ],
 })
 export class KeyboardComponent implements OnInit {
+
+    readonly panZoom = viewChild(PanZoomComponent);
+    readonly panzoomModel = signal<PanZoomModel>(undefined!);
 
     /**
      * Constructor
@@ -26,5 +35,14 @@ export class KeyboardComponent implements OnInit {
     constructor() { }
 
     ngOnInit(): void {
+    }
+
+
+    scalePerZoomLevel(): void {
+
+    }
+
+    neutralZoomLevel(): void {
+
     }
 }
